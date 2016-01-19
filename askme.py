f2108a5efc0e637fe20097d6e442aa7656fbf052
Joe Connor <joe.connor@factorymedia.com>
2016-01-19 13:58:22 +0800

    My first Python Script

---
+++ askme.py
@@ -0,0 +1,15 @@
+def askMe():
+  name = input("Hi there, whats your name? ")
+  
+  place = input("Nice to meet you " + name
+                + " and where are you from? ")
+  
+  age = input("Cool, I always fancied visiting " + place
+              + ". So how old are you anyway? ")
+  
+  print("Wow so you're " + age
+        + " years old and from " + place
+        + " and your name is " + name
+        + ". Pretty neat!")
+
+askMe()
